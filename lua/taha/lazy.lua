d2d54cc325935dae776d9868499c0d87c3940d0e
taham8875 <taha.m8875@gmail.com>
2025-03-25 21:44:14 +0200

    add mini.ai, surround and todo comments

--- lua/taha/lazy.lua
+++ lua/taha/lazy.lua
@@ -5,9 +5,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { "Failed to clone lazy.nvim:\n", "errormsg" },
+      { out, "warningmsg" },
+      { "\npress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -17,15 +17,30 @@ end
 -- put lazy into the runtimepath for neovim
 vim.opt.runtimepath:prepend(lazypath)
 
--- Setup lazy.nvim
+-- setup lazy.nvim
 require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "taha.plugins" },
-	},
-	-- some plugins are simple, not worth to move them to the plugins folders
-	'tpope/vim-sleuth', -- auto detect what the current file uses (tabs vs spaces, 2 or 4 or 8 space indent
-	-- Configure any other settings here. See the documentation for more details.
+		-- some plugins are simple, not worth to move them to the plugins folders
+		'tpope/vim-sleuth', -- auto detect what the current file uses (tabs vs spaces, 2 or 4 or 8 space indent
+
+		-- highlight todo, notes, etc in comments
+		{ 'folke/todo-comments.nvim', event = 'vimenter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+		{
+		  "kylechui/nvim-surround",
+		  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		  event = "VeryLazy",
+		  config = function()
+		    require("nvim-surround").setup({
+		      -- Configuration here, or leave empty to use defaults
+		    })
+		  end
+		}
+	      },
+	
+	-- configure any other settings here. see the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
--- lua/taha/plugins/mini.lua
+++ lua/taha/plugins/mini.lua
@@ -4,6 +4,8 @@ return {
 		config = function()
 			local statusline = require 'mini.statusline'
 			statusline.setup {use_icons = true}
+
+			require('mini.ai').setup { n_lines = 500 }
 		end
 	}
 }
