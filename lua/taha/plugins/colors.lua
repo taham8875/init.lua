0979d20ca02b7e0d7e9d2470943718a1cbffe1e7
taham8875 <taha.m8875@gmail.com>
2025-04-30 04:37:57 +0300

    catppuccin is the answer

--- lua/taha/plugins/colors.lua
+++ lua/taha/plugins/colors.lua
@@ -1,16 +1,31 @@
 return {
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			styles = {
+	-- 				italic = false,
+	-- 			},
+	-- 		})
+	-- 		vim.cmd.colorscheme("rose-pine")
+	-- 	end,
+	-- },
 	{
-		"rose-pine/neovim",
+		"catppuccin/nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		config = function()
-			require("rose-pine").setup({
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
 				styles = {
-					italic = false,
+					comments = {},
 				},
 			})
-			vim.cmd.colorscheme("rose-pine")
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
